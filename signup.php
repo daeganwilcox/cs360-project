cb184226f5ed4f6b691b0fe8b391a2a74788d8df
superkingjunior <72706211+superkingjunior@users.noreply.github.com>
2020-10-24 18:19:17 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -15,11 +15,11 @@ $res = registerUser($db, $_POST);
 
 if($res){ //page if registration successful:
   print "<H2>New Account Created</H2>";
-  print "<p>Your account has been successfully created and a verification email have been sent to your email address. Please verify your email, then log in using <a href='login.html'>this</a> link.</p>"; //TODO change login link
+  print "<p>Your account has been successfully created and a verification email have been sent to your email address. Please verify your email, then log in using <a href='http://wilcda01-workout-app.herokuapp.com/html/login.html'>this</a> link.</p>";
 }
 else{ //page if registration unsuccessful:
   print "<H2>Account already exists!</H2>";
-  print "<p>The login you entered already exists. Try logging in <a href='login.html'>here</a>, or try signing up with a different login <a href='signup.html'>here</a>.</p>"; //TODO change login and signup link
+  print "<p>The login you entered already exists. Try logging in <a href='http://wilcda01-workout-app.herokuapp.com/html/login.html'>here</a>, or try signing up with a different login <a href='http://wilcda01-workout-app.herokuapp.com/html/signup.html'>here</a>.</p>";
 }
 
 ?>
