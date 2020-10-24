24253ff1d21787183390e267fe00d1a746928726
superkingjunior <72706211+superkingjunior@users.noreply.github.com>
2020-10-24 18:31:04 -0400

    Update login-signup-utils.php

--- login-signup-utils.php
+++ login-signup-utils.php
@@ -59,7 +59,7 @@ function registerUser($db, $input){
   $headers = "MIME-Version: 1.0" . "\r\n";
   $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-  $headers .= "From: Fitness Site <DO NOT REPLY!>" . "\r\n";
+  $headers .= "From: Fitness Site <notARealEmail@gettysburg.edu>" . "\r\n";
 
   mail($email, $subject, $message, $headers); //sends the email
 
