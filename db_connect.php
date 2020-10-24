4316f06b376a970446427dbdc6b5bfe111851199
superkingjunior <72706211+superkingjunior@users.noreply.github.com>
2020-10-24 12:48:27 -0400

    Create db_connect.php
    connects to our database

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "ada.cc.gettysburg.edu";
+$dbase = "f20_4";
+$user = "presco01";
+$pass = "presco01";
+
+try{
+  $db = new PDO("mysql:host=$host;dbname=$dbase", $user, $pass);
+}
+catch (PDOException $e){
+  die("ERROR connecting to MySQL database " . $e->getMessage());
+}
+
+?>
