9db4798b9d63f636b741e692eb161d19d6451f6f
mirandaricky9 <mirari01@gettysburg.edu>
2020-11-01 15:55:37 -0800

    changed redirection for logout

--- logout.php
+++ logout.php
@@ -2,5 +2,5 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/index.php"); //redirects to original page
+header("Location: http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/"); //redirects to original page
 ?>
