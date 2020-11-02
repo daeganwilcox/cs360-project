db0904350044fcc50d0589007696b5e757b17ced
Superkingjunior <pressercc@gmail.com>
2020-11-01 19:25:22 -0500

    fix?

--- userhome.php
+++ userhome.php
@@ -25,7 +25,26 @@ session_start();
                 <h2>Programs</h2>
               </div>
               <?php
-              getStartedPrograms();
+              $qRes = getStartedPrograms();
+              if($qRes == FALSE){
+                print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
+                return FALSE;
+              }
+              if($qRes-> rowCount() == 0){
+                print "<H5>You haven't started any programs yet.</H5>";
+                return TRUE;
+              }
+              for($i = 0; $i < 5 && $row = $qRes->fetch(); $i++){
+                $name = $row['name'];
+                $id = $row['id'];
+                print "<div class="card-body">";
+                print "<A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id>";
+                print "<H4 class="card-text">$name</H4>";
+                print "</A>";
+                print "<div class="d-flex justify-content-between align-items-center">";
+                print "</div>";
+                print "</div>";
+              }
               ?>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Make New Program</button>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Find New Programs</button>
