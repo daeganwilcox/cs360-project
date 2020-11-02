fc45aeab173f4d639b5f39f5e5cf8019758e517a
Superkingjunior <pressercc@gmail.com>
2020-11-01 19:43:22 -0500

    fix?

--- userhome.php
+++ userhome.php
@@ -23,28 +23,29 @@ session_start();
               <div class="programs">
                 <h2>Programs</h2>
               </div>
-              <?PHP
+              <?php
               $uid = $_SESSION['username'];
               $qStr = "SELECT name, programID AS id FROM ((SELECT DISTINCT programID FROM completed WHERE userID = '$uid' ORDER BY date_time) NATURAL JOIN program);";
               $qRes = $db->query($qStr);
               if($qRes == FALSE){
                 print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
               }
-              /*if($qRes-> rowCount() == 0){
+              else if($qRes-> rowCount() == 0){
                 print "<H5>You haven't started any programs yet.</H5>";
               }
-              for($i = 0; $i < 5 && $row = $qRes->fetch(); $i++){
-                $name = $row['name'];
-                $id = $row['id'];
-                print "<div class="card-body">";
-                print "<A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id>";
-                print "<H4 class="card-text">$name</H4>";
-                print "</A>";
-                print "<div class="d-flex justify-content-between align-items-center">";
-                print "</div>";
-                print "</div>";
-	      }
-	       */
+              else{
+                for($i = 0; $i < 5 && $row = $qRes->fetch(); $i++){
+                  $name = $row['name'];
+                  $id = $row['id'];
+                  print "<div class="card-body">";
+                  print "<A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id>";
+                  print "<H4 class="card-text">$name</H4>";
+                  print "</A>";
+                  print "<div class="d-flex justify-content-between align-items-center">";
+                  print "</div>";
+                  print "</div>";
+                }
+              }
               ?>
               <div class="card-body">
                 <a href="momcardio.html">
