43dbff5ad907131e2282adaaeaf3c2868d83c383
Superkingjunior <pressercc@gmail.com>
2020-11-08 12:42:51 -0500

    debug

--- userhome.php
+++ userhome.php
@@ -26,7 +26,7 @@ session_start();
               </div>
               <?php
               $uid = $_SESSION['username'];
-              print "<p>" . getPrograms($uid) . "</p>";
+              print "<p>" . getPrograms($uid) . "</p></div>";
               ?>
             </div>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Make New Program</button>
