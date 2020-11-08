f2ecd4ab90e7cab87cd87f4b3b14e61ac175e5f9
mirandaricky9 <mirari01@gettysburg.edu>
2020-11-08 12:04:53 -0800

    adding friend functionality

--- addFriend.php
+++ addFriend.php
@@ -1,4 +1,22 @@
 <?php
 include_once("db_connect.php")
+$uid = $_SESSION['username'];
+$userFriend = $_POST['userFriend'];
 
+$qStr = "SELECT username FROM user WHERE username='$userFriend';";
+$qRes = $db->query($qStr);
+
+if($qRes-> rowCount() == 0){
+    print "user does not exist, please try again";
+}
+else{
+    $qStr = "INSERT INTO friend ('user1', 'user2') VALUES ($uid, $userFriend)";
+    $qRes = $db->query($qStr);
+    if($qRes == FALSE){
+        print "Failure to add $userFriend";
+    }
+    else{
+        print "Successfully added $userFriend, go back to your homepage to see an updated friends list";
+    }
+}
 ?>
