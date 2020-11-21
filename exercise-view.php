09eeed792dbc4daa35bfd5b763b803605913cb4a
Superkingjunior <pressercc@gmail.com>
2020-11-21 12:25:36 -0500

    fix?

--- exercise-view.php
+++ exercise-view.php
@@ -12,32 +12,34 @@ include_once("db_connect.php");
       print "<H1>Missing Exercise ID</H1>";
       print "<H5>This link doesn't point to a specific exercise. Please make sure you have the correct address.</H5>";
     }
-
-    $qStr = "SELECT * FROM exercise WHERE exerciseID = $eid;";
-    $qRes = $db->query($qStr);
-
-    //broken Query
-    if($qRes == FALSE){
-      printSQLError(1);
-    }
-
-    $row = $qRes->fetch();
-
-    if($row = FALSE){
-      print "<H1>Exercise Does Not Exist</H1>";
-      print "<H5>This link points to a exercise which doesn't exist. Please make sure you have the correct address.</H5>";
-    }
     else{
-      $name = $row['name'];
-      $desc = $row['description'];
-      $met = $row['met'];
-      $usesReps = $row['usesReps'] ? "" : "n't";
-      $usesWeight = $row['usesWeight'] ? "" : "n't";
-      print "<H1>$name</H1>";
-      print "<H5>$desc</H5>";
-      print "<H6>Metabolic Equivalent of Task (MET): $met</H6>";
-      print "<H6>This exercise does$usesReps use reps.</H6>";
-      print "<H6>This exercise does$usesWeight use weight.</H6>";
+      $qStr = "SELECT * FROM exercise WHERE exerciseID = $eid;";
+      $qRes = $db->query($qStr);
+
+      //broken Query
+      if($qRes == FALSE){
+        printSQLError(1);
+      }
+      else{
+        $row = $qRes->fetch();
+
+        if($row = FALSE){
+          print "<H1>Exercise Does Not Exist</H1>";
+          print "<H5>This link points to a exercise which doesn't exist. Please make sure you have the correct address.</H5>";
+        }
+        else{
+          $name = $row['name'];
+          $desc = $row['description'];
+          $met = $row['met'];
+          $usesReps = $row['usesReps'] ? "" : "n't";
+          $usesWeight = $row['usesWeight'] ? "" : "n't";
+          print "<H1>$name</H1>";
+          print "<H5>$desc</H5>";
+          print "<H6>Metabolic Equivalent of Task (MET): $met</H6>";
+          print "<H6>This exercise does$usesReps use reps.</H6>";
+          print "<H6>This exercise does$usesWeight use weight.</H6>";
+        }
+      }
     }
     ?>
   </body>
