f523f002fd5a32301ff5fbbed7b612f33d4cc1e8
mirandaricky9 <mirari01@gettysburg.edu>
2020-11-21 11:57:31 -0800

    View All Programs functionalities

--- viewAllPrograms.php
+++ viewAllPrograms.php
@@ -13,56 +13,65 @@ $userpresent = $uid != NULL;
     <main role="main">
         <div class="row">
             <div class="col-md-2">
-                <h2>Programs you are apart of:</h2>
-                
-                <?php
-                print $uid;
-                $qStr = "SELECT name, programID AS id FROM (SELECT DISTINCT programID FROM completed WHERE userID = '$uid' ORDER BY date_time) AS uComp NATURAL JOIN program;";
-                $qRes = $db->query($qStr);
-                if ($qRes == FALSE) {
-                    print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
-                } else if ($qRes->rowCount() == 0) {
-                    print "<H5>You haven't started any programs yet.</H5>";
-                } else {
-                    while ($qRes->fetch() != FALSE) {
-                        $row = $qRes->fetch();
-                        $name = $row['name'];
-                        $id = $row['id'];
-                        print "<div class='card-body'>";
-                        print "<A href='http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id'>";
-                        print "<H4 class='card-tex'>$name</H4>";
-                        print "</A>";
-                        print "<div class='d-flex justify-content-between align-items-center'>";
-                        print "</div>";
-                        print "</div>";
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-object">
+                        <h2>Programs you are a part of:</h2>
+                    </div>
+
+                    <?php
+                    print $uid;
+                    $qStr = "SELECT name, programID AS id FROM (SELECT DISTINCT programID FROM completed WHERE userID = '$uid' ORDER BY date_time) AS uComp NATURAL JOIN program;";
+                    $qRes = $db->query($qStr);
+                    if ($qRes == FALSE) {
+                        print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
+                    } else if ($qRes->rowCount() == 0) {
+                        print "<H5>You haven't started any programs yet.</H5>";
+                    } else {
+                        while ($qRes->fetch() != FALSE) {
+                            $row = $qRes->fetch();
+                            $name = $row['name'];
+                            $id = $row['id'];
+                            print "<div class='card-body'>";
+                            print "<A href='http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id'>";
+                            print "<H4 class='card-tex'>$name</H4>";
+                            print "</A>";
+                            print "<div class='d-flex justify-content-between align-items-center'>";
+                            print "</div>";
+                            print "</div>";
+                        }
                     }
-                }
-                ?>
+                    ?>
+                </div>
             </div>
+
             <div class="col-md-2">
-                <h2>Programs you have created:</h2>
-                <?php
-                $qStr = "SELECT name, programID AS id FROM program WHERE creatorID = $uid;";
-                $qRes = $db->query($qStr);
-                if ($qRes == FALSE) {
-                    print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
-                } else if ($qRes->rowCount() == 0) {
-                    print "<H5>You haven't started any programs yet.</H5>";
-                } else {
-                    while ($qRes->fetch()) {
-                        $row = $qRes->fetch();
-                        $name = $row['name'];
-                        $id = $row['id'];
-                        print "<div class='card-body'>";
-                        print "<A href='http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id'>";
-                        print "<H4 class='card-tex'>$name</H4>";
-                        print "</A>";
-                        print "<div class='d-flex justify-content-between align-items-center'>";
-                        print "</div>";
-                        print "</div>";
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-object">
+                        <h2>Programs you have created:</h2>
+                    </div>
+                    <?php
+                    $qStr = "SELECT name, programID AS id FROM program WHERE creatorID = '$uid';";
+                    $qRes = $db->query($qStr);
+                    if ($qRes == FALSE) {
+                        print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
+                    } else if ($qRes->rowCount() == 0) {
+                        print "<H5>You haven't started any programs yet.</H5>";
+                    } else {
+                        while ($qRes->fetch()) {
+                            $row = $qRes->fetch();
+                            $name = $row['name'];
+                            $id = $row['id'];
+                            print "<div class='card-body'>";
+                            print "<A href='http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id'>";
+                            print "<H4 class='card-tex'>$name</H4>";
+                            print "</A>";
+                            print "<div class='d-flex justify-content-between align-items-center'>";
+                            print "</div>";
+                            print "</div>";
+                        }
                     }
-                }
-                ?>
+                    ?>
+                </div>
             </div>
         </div>
     </main>
