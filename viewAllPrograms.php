87d36015cb0f2ea1e7a71c97e88341a82e6a9d06
mirandaricky9 <mirari01@gettysburg.edu>
2020-11-22 10:02:11 -0800

    View all programs functionality

--- viewAllPrograms.php
+++ viewAllPrograms.php
@@ -12,43 +12,53 @@ $userpresent = $uid != NULL;
 <body>
     <main role="main">
         <div class="row">
-            <div class="col-md-2 col-centered">
-                <div class="card mb-4 shadow-sm">
-                    <div class="card-object">
-                        <h2>Programs you are a part of:</h2>
-                    </div>
+            <div class="card mb-4 shadow-sm">
+                <h2>Programs you are a part of:</h2>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>description</th>
+                        <th>Last Edited</th>
+                    </tr>
                     <?php
-                    $qStr = "SELECT name, programID AS id FROM (SELECT DISTINCT programID FROM completed WHERE userID = '$uid' ORDER BY date_time) AS uComp NATURAL JOIN program;";
+                    $qStr = "SELECT name, programID as id, description,MAX(date_time) AS date FROM (SELECT DISTINCT programID, date_time FR
+                    OM completed WHERE userID = 'superkingjunior' ORDER BY date_time) AS uComp NATURAL JOIN program;";
                     $qRes = $db->query($qStr);
                     if ($qRes == FALSE) {
-                        print "hello";
                         print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
                     } else if ($qRes->rowCount() == 0) {
                         print "<H5>You haven't started any programs yet.</H5>";
                     } else {
                         while ($row = $qRes->fetch()) {
                             $name = $row['name'];
+                            $description = $row['description'];
+                            $date = $row['date'];
                             $id = $row['id'];
-                            print "<div class='card-body'>";
+                            print "<tr>";
                             print "<A href='http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id'>";
-                            print "<H4 class='card-tex'>$name</H4>";
+                            print "<td>$name</tr>";
                             print "</A>";
-                            print "<div class='d-flex justify-content-between align-items-center'>";
-                            print "</div>";
-                            print "</div>";
+                            print "<td>$description</td>";
+                            print "<td>$date</td>";
+                            print "</tr>";
                         }
                     }
                     ?>
-                </div>
+                </table>
             </div>
+        </div>
 
-            <div class="col-md-2">
-                <div class="card mb-4 shadow-sm">
-                    <div class="card-object">
-                        <h2>Programs you have created:</h2>
-                    </div>
+        <div class="col-md-2">
+            <div class="card mb-4 shadow-sm">
+                <h2>Programs you created:</h2>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>description</th>
+                        <th>Date Created</th>
+                    </tr>
                     <?php
-                    $qStr = "SELECT name, programID AS id FROM program WHERE creatorID = '$uid';";
+                    $qStr = "SELECT name, programID as id, description, date_created AS date FROM program WHERE creatorID = '$uid';";
                     $qRes = $db->query($qStr);
                     if ($qRes == FALSE) {
                         print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
@@ -57,19 +67,21 @@ $userpresent = $uid != NULL;
                     } else {
                         while ($row = $qRes->fetch()) {
                             $name = $row['name'];
+                            $description = $row['description'];
+                            $date = $row['date'];
                             $id = $row['id'];
-                            print "<div class='card-body'>";
+                            print "<tr>";
                             print "<A href='http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id'>";
-                            print "<H4 class='card-tex'>$name</H4>";
+                            print "<td>$name</tr>";
                             print "</A>";
-                            print "<div class='d-flex justify-content-between align-items-center'>";
-                            print "</div>";
-                            print "</div>";
+                            print "<td>$description</td>";
+                            print "<td>$date</td>";
+                            print "</tr>";
                         }
                     }
                     ?>
-                </div>
             </div>
+        </div>
         </div>
     </main>
 </body>
