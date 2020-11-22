b126adb5ee822b341dad9cffeef20a03cbc92399
mirandaricky9 <mirari01@gettysburg.edu>
2020-11-21 20:13:48 -0800

    View all programs functionality

--- viewAllPrograms.php
+++ viewAllPrograms.php
@@ -19,9 +19,7 @@ $userpresent = $uid != NULL;
                     </div>
                     <?php
                     $qStr = "SELECT name, programID AS id FROM (SELECT DISTINCT programID FROM completed WHERE userID = '$uid' ORDER BY date_time) AS uComp NATURAL JOIN program;";
-                    print $qStr;
                     $qRes = $db->query($qStr);
-                    //print $qRes;
                     if ($qRes == FALSE) {
                         print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
                     } else if ($qRes->rowCount() == 0) {
