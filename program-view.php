af189d86e5d541973440620e72aa9723231b8aef
Superkingjunior <pressercc@gmail.com>
2020-11-22 12:27:43 -0500

    fix?

--- program-view.php
+++ program-view.php
@@ -1,3 +1,8 @@
+<script>
+    if(typeof window.history.pushState == 'function') {
+        window.history.pushState({}, "Hide", '<?php echo $_SERVER['PHP_SELF'];?>');
+    }
+</script>
 <?php
 include_once("db_connect.php");
 session_start();
