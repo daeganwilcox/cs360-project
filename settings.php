545fc4fd8ae609cc93c7d3f304d246cc6ceb4a3f
Superkingjunior <pressercc@gmail.com>
2020-11-25 13:51:38 -0500

    fix

--- settings.php
+++ settings.php
@@ -5,7 +5,7 @@ $uid = $_SESSION['username'];
 
 <!doctype html>
 <html lang="en">
-  <?php include("base.php") ?>
+  <?php include("base.php"); ?>
   <body>
     <?php
     if($uid == NULL){
--- userhome.php
+++ userhome.php
@@ -8,7 +8,7 @@ $uid = $_SESSION['username'];
 <!doctype html>
 <html lang="en">
 
-<?php include("base.php") ?>
+<?php include("base.php"); ?>
 
 <body>
   <main role="main">
