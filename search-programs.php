eefb01659b07a85449a26be931afc0d6abcaf19a
Superkingjunior <pressercc@gmail.com>
2020-11-25 13:57:53 -0500

    added links

--- search-programs.php
+++ search-programs.php
@@ -10,7 +10,7 @@ $search = $_POST['search'];
   <?php
     function printList($db, $search){
       //if search is null, search is empty so I don't have to check it
-      $qStr = "SELECT name, date_created, AVG(rating) AS avg FROM program LEFT JOIN reviews ON program.programID = reviews.programID WHERE `name` LIKE '%$search%' GROUP BY program.programID, date_created ORDER BY AVG(rating) DESC;";
+      $qStr = "SELECT name, program.programID AS id, date_created, AVG(rating) AS avg FROM program LEFT JOIN reviews ON program.programID = reviews.programID WHERE `name` LIKE '%%' GROUP BY program.programID ORDER BY AVG(rating) DESC";
       $qRes = $db->query($qStr);
       if($qRes == FALSE){
         printSQLError("for searching");
@@ -23,11 +23,12 @@ $search = $_POST['search'];
       print "<TABLE border='1' class='table'>";
       print "<TR><TH>Program Name</TH><TH>Average Rating</TH><TH>Date Created</TH></TR>";
       while($row = $qRes->fetch()){
+        $id = $row['id'];
         $name = $row['name'];
         $avg = round($row['avg'], 2);
         $date = $row['date_created'];
         print "<TR>";
-        print "<TD>$name</TD>";
+        print "<TD><a href='http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id'>$name</a></TD>";
         print "<TD>$avg</TD>";
         print "<TD>$date</TD>";
         print "</TR>";
