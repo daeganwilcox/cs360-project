3748f430a94c2e6b0b681f5b477a79085834867b
mirandaricky9 <mirari01@gettysburg.edu>
2020-11-26 08:42:24 -0800

    Adding public page and Profile view functionality

--- base.php
+++ base.php
@@ -93,6 +93,9 @@ $uid = $_SESSION['username'];
           <img src="https://img.icons8.com/windows/32/000000/dumbbell.png" />
           <strong>Home</strong>
         </a>
+        <a href="http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/publicPage.php" class="navbar-brand d-flex align-items-center">
+          <strong>Profiles</strong>
+        </a>
         <?php
         if ($uid != null) {
           print "<a href='http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/userhome.php' class='navbar-brand d-flex align-items-center'>";
