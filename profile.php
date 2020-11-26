368f235ef9f5a2319e84b2621b9df7998c591da5
mirandaricky9 <mirari01@gettysburg.edu>
2020-11-26 09:04:44 -0800

    Adding public page and Profile view functionality

--- profile.php
+++ profile.php
@@ -73,10 +73,12 @@ $eid = $_GET['user']
 
                         <div class="col-md-4">
                             <div class="card mb-4 shadow-sm">
-                                <h2> Programs </h2>
+                                <div class="card-object">
+                                    <h2> Programs </h2>
+                                </div>
                                 <div>
                                     <?php
-                                    $qStr = "SELECT name, programID AS id FROM (SELECT DISTINCT programID FROM completed WHERE userID = '$uid' ORDER BY date_time DESC) AS uComp NATURAL JOIN program;";
+                                    $qStr = "SELECT name, programID AS id FROM (SELECT DISTINCT programID FROM completed WHERE userID = '$eid' ORDER BY date_time DESC) AS uComp NATURAL JOIN program;";
                                     $qRes = $db->query($qStr);
                                     if ($qRes == FALSE) {
                                         print "<H5>There was a MySQL query error. Please contact one of our developers using our Contact Us page.</H5>";
@@ -86,9 +88,13 @@ $eid = $_GET['user']
                                         for ($i = 0; $i < 5 && $row = $qRes->fetch(); $i++) {
                                             $name = $row['name'];
                                             $id = $row['id'];
+                                            print "<div class='card-body'>";
                                             print "<A href='http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/program-view.php/?id=$id'>";
-                                            print "<p class='card-tex'>$name</p>";
+                                            print "<H4 class='card-tex'>$name</H4>";
                                             print "</A>";
+                                            print "<div class='d-flex justify-content-between align-items-center'>";
+                                            print "</div>";
+                                            print "</div>";
                                         }
                                     }
                                     ?>
