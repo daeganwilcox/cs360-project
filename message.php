d6587e86c809a1832201622a4200833f5d6b60a6
Daegan Wilcox <daeganwilcox@icloud.com>
2020-11-28 15:31:54 -0500

    Update message.php

--- message.php
+++ message.php
@@ -2,6 +2,10 @@
 include_once("db_connect.php");
 session_start();
 $uid = $_SESSION['username'];
+// needs to check the user accessing this page, and should have a POST where the user is given. right here should probably re indentify if they are still friends
+
+// - user1 and user2 have a specific user message log in the database, pull here every 5 seconds
+// - when a user sends a message, add to message log
 ?>
 
 
@@ -9,3 +13,4 @@ $uid = $_SESSION['username'];
 <html lang="en">
 
 <?php include("base.php"); ?>
+  
