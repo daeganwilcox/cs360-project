2d70617cb90b1b136e374c491abff0550dd23809
mirandaricky9 <mirari01@gettysburg.edu>
2020-11-28 11:41:36 -0800

    Adding user check-in at home page

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+session_start();
+$uid = $_SESSION['username'];
+if ($uid != null){
+    header("Location: http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/userhome.php");
+}
 include_once("html/index.html");
 ?>
