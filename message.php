a61e75c8d6e1e0e79f80ff3c280a49d60b8a41d1
Daegan Wilcox <daeganwilcox@icloud.com>
2020-12-03 16:09:35 -0500

    Update message.php

--- message.php
+++ message.php
@@ -6,11 +6,30 @@ $uid = $_SESSION['username'];
 
 // - user1 and user2 have a specific user message log in the database, pull here every 5 seconds
 // - when a user sends a message, add to message log
+
+// when user starts the page, it gets the dialogue. thus, when they send a message, it should update the dialogue
 ?>
+
+
 
 
 <!doctype html>
 <html lang="en">
 
 <?php include("base.php"); ?>
+  
+<?php 
+  print "<div style='width: 80%; height: 80%; overflow: auto;";
+  
+  
+  
+  print "<div>"
+    
+    
+  print "<form class='form-signin' method='post' action='message.php'>";
+  print "<textarea id='msgInput' class='form-control' placeholder='Message text' required></textarea>";
+  print "<button class='btn btn-lg btn-primary btn-block' type='submit'>Send Message</button>";
+  print "</form>";
+  
+?>
   
