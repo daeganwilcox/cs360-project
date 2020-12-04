f018925c99a242a34416ae1e24d3d6c61c8e6b85
Daegan Wilcox <daeganwilcox@icloud.com>
2020-12-04 17:44:46 -0500

    Update contactUs.php

--- contactUs.php
+++ contactUs.php
@@ -5,27 +5,24 @@ session_start();
 $uid = $_SESSION['username'];
 ?>
 
-<!doctype html>
-<html lang="en">
+<?
+   php include("base.php"); 
+       
+   print "<container>";
+   print "<div style='text-align-center; margin: auto;'>";
+   print "<H2>Contact us!</H2>";
+   print "<FORM name='fmMail' method='POST' action='contactAction.php'>";
+   print "Name:<INPUT type='text' name='tfName' size='30' placeholder='Enter your name' /><BR /><BR />";
+   print "Email:<INPUT type='email' name='tfEmail' size='30' placeholder='Enter your email' /><BR /><BR />";
+   print "Message:<TEXTAREA name='taContent' rows='5' cols='60'> </TEXTAREA>";    
+   print "";
+   print "<INPUT type='submit' value='Email us!' />";
+   print "</div>";
+   print "</container>";
+       
+       
+       
+       
+       
+?>
 
-<?php include("base.php"); ?>
-
-<BODY>
-<H2>Contact us!</H2>
-
-<FORM name='fmMail' method='POST' action='contactAction.php'>
-<INPUT type='text' name='tfName' size='30'
-       placeholder='Enter your name' /><BR /><BR />
-<INPUT type='text' name='tfEmail' size='30'
-       placeholder='Enter your email' /><BR /><BR />
-
-Content: <BR />
-<TEXTAREA name='taContent' rows='5' cols='60'> </TEXTAREA>
-<BR />
-<BR />
-
-<INPUT type='submit' value='Email us!' />
-</FORM>
-
-</BODY>
-</HTML>
