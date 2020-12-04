40af84c6dd4fd70891cbcbfd9a6b9841d94ba01e
Daegan Wilcox <daeganwilcox@icloud.com>
2020-12-04 17:54:46 -0500

    Update contactUs.php

--- contactUs.php
+++ contactUs.php
@@ -15,7 +15,6 @@ $uid = $_SESSION['username'];
    print "<FORM name='fmMail' method='POST' action='contactAction.php'>";
    print "<INPUT style='width: 60%; margin: auto;' type='text' name='tfName' size='30' placeholder='Enter your name' /><BR /><BR />";
    print "<INPUT style='width: 60%; margin: auto;' type='email' name='tfEmail' size='30' placeholder='Enter your email' /><BR /><BR />";
-   print "Message:";
    print "<TEXTAREA name='taContent' rows='5' style='width: 60%; cols='60' placeholder='Enter your message'> </TEXTAREA>";    
    print "<button class='btn btn-lg btn-primary btn-block' style='width: 60%; margin: auto; margin-top: 5px;'type='submit'>Email Us!</button>";
    print "</FORM>";
