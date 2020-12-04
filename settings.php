b1237bcd3b999cd7478bf8928b7a8eebb014a7ff
Daegan Wilcox <daeganwilcox@icloud.com>
2020-12-04 17:14:05 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -22,10 +22,11 @@ $uid = $_SESSION['username'];
       print "<container>";
       print "<div style='text-align: center; margin: auto; width: 80%'>";
       print "<H1>Account Settings for $uid</H1>";
-      print "<UL>";
-      print "<LI><H3><A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/change-pass.php>Change Password</A></H3></LI>";
-      print "<LI><H3><A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/change-img.php>Change Profile Picture</A></H3></LI>";
-      print "<LI><H3><A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/editSettings.php>Change Height/Weight</A></H3></LI>";
+      print "<UL style='list-group'>";
+      print "<LI style='list-group-item'><H3><A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/change-pass.php>Change Password</A></H3></LI>";
+      print "<LI style='list-group-item'><H3><A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/change-pass.php>Change Password</A></H3></LI>";
+      print "<LI style='list-group-item'><H3><A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/change-img.php>Change Profile Picture</A></H3></LI>";
+      print "<LI style='list-group-item'><H3><A href=http://www.cs.gettysburg.edu/~mirari01/cs360project/cs360-project/editSettings.php>Change Height/Weight</A></H3></LI>";
       print "</UL>";
       print "</container>";
     }
