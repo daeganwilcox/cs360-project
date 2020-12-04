981b7409ebe1df72f94baa0bcb4a486b122313fe
Daegan Wilcox <daeganwilcox@icloud.com>
2020-12-03 19:21:17 -0500

    Update message.php

--- message.php
+++ message.php
@@ -43,9 +43,9 @@ if ($qRes1-> rowCount() == 0) {
     $date = $row['date'];
     
     if ($sender==$uid) {
-      print "<h3 style='text-align: right; max-width: 50%;'>$message</h1>";
+      print "<h3 style='text-align: right; max-width: 50%; margin: auto;'>$message</h1>";
     } else if ($sender==$friend) {
-      print "<h3 style='text-align: left; max-width: 50%;'>$message</h1>"; 
+      print "<h3 style='text-align: left; max-width: 50%; margin: auto'>$message</h1>"; 
     }
   }
   
